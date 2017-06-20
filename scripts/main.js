5e28120eed664b81da094fd594723970a0c8c994
Sean X. Rich <seanxrich@icloud.com>
2017-06-20 16:43:17 -0400

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $('#search_term').keyup(function(){
+
+      var searchField = $('#search_term').val();
+      var regex = new RegExp(searchField, "i");
+      var output = '';
+
+      $.getJSON('data/blues.json', function(data) {
+        $.each(data, function(key, val){
+          if ((val.Artist.search(regex) != -1) || (val.Title.search(regex) != -1)) {
+            output += '<div class="result_artist">' + val.Artist + '</div>';
+            output += '<div class="result_title">' + val.Title + '</div>'
+          }
+        });
+        $('#search_results').html(output);
+      });
+  });
+});
