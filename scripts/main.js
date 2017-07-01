6833a470aff01eda55b67fc01de89a10eaf5b3db
Sean X. Rich <seanxrich@users.noreply.github.com>
2017-07-01 15:34:27 -0400

    Style changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('#search_term').keyup(function(){
+  $('#search_term').keyup(function () {
 
       var searchField = $('#search_term').val();
-      var regex = new RegExp(searchField, "i");
+      var regex = new RegExp(searchField, 'i');
       var output = '';
 
-      $.getJSON('data/blues.json', function(data) {
-        $.each(data, function(key, val){
+      $.getJSON('data/blues.json', function (data) {
+        $.each(data, function (key, val) {
           if ((val.Artist.search(regex) != -1) || (val.Title.search(regex) != -1)) {
             output += '<div class="result_artist">' + val.Artist + '</div>';
-            output += '<div class="result_title">' + val.Title + '</div>'
+            output += '<div class="result_title">' + val.Title + '</div>';
           }
-        });
+        }
+      );
         $('#search_results').html(output);
       });
-  });
+    });
 });
