925c9cbbe67d3e6fa6c094821d5301faf18843ee
moonlight-blade <32277072+moonlight-blade@users.noreply.github.com>
2017-09-26 07:53:15 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
       $.getJSON('data/blues.json', function (data) {
         $.each(data, function (key, val) {
-          if ((val.Artist.search(regex) != -1) || (val.Title.search(regex) != -1)) {
+          if ((val.quiz.search(regex) != -1) || (val.Title.search(regex) != -1)) {
             output += '<div class="result_quiz">' + val.quiz + '</div>';
             output += '<div class="result_answer">' + val.answer + '</div>';
           }
